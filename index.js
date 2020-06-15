11f519b554cd034423b80f5f8becba99261f9613
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-06-15 13:13:42 +0200

    Added mongo support

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import express from 'express';
+import mongodb from 'mongodb';
+
+const MongoClient = mongodb.MongoClient;
+
+const url = 'mongodb://localhost';
+let db = null;
+let collection = null;
+
+MongoClient.connect(url, { useUnifiedTopology: true }, async (err, client) => {
+  if(err) {
+    console.error(err);
+    return;
+  }
+  db = client.db('forest');
+  collection = db.collection('entries');
+});
 
 const app = express();
 app.set('views', './views');
@@ -6,19 +22,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('assets'));
 
-const data = 
-{
-  links:
-    [
-      {
-        url: 'https://google.com',
-        text: 'Google'
-      },
-    ]
-};
-
+;
 app.get('/', function (req, res) {
   res.render('index', data);
+});
+app.get('/:entry', async (request, response) => {
+  const data = await collection.findOne({ name: request.params.entry });
+  if(data === null) {
+    response.status(404);
+    response.render('404');
+    return;
+  }
+  response.render('index', data);
 });
 
 app.listen(3000);
