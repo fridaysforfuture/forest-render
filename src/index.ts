b8007a1c8a27538515d6b618dabc0d472744e427
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-06-15 14:42:16 +0200

    Finished typescript setup

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,10 @@ import mongodb from 'mongodb';
 const MongoClient = mongodb.MongoClient;
 
 const url = 'mongodb://localhost';
-let db = null;
-let collection = null;
 
-MongoClient.connect(url, { useUnifiedTopology: true }, async (err, client) => {
-  if(err) {
-    console.error(err);
-    return;
-  }
-  db = client.db('forest');
-  collection = db.collection('entries');
-});
+const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+const db = client.db('forest');
+const collection = db.collection('entries')
 
 const app = express();
 app.set('views', './views');
@@ -22,10 +15,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('assets'));
 
-;
-app.get('/', function (req, res) {
-  res.render('index', data);
-});
 app.get('/:entry', async (request, response) => {
   const data = await collection.findOne({ name: request.params.entry });
   if(data === null) {
